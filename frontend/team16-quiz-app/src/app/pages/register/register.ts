f6e50faca9ceef281f399180b918cd3a75190493
amirs <amirsabour874572@gmail.com>
2026-06-03 12:45:22 +0200

    pages erstellt (noch leer)

---
+++ frontend/team16-quiz-app/src/app/pages/register/register.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {Routes} from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  templateUrl: './register.html',
+  styleUrl: './register.scss',
+})
+export class Register {}
