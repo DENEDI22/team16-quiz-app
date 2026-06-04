7aab0a6914fbc3c104133f777c59be4d5ae834be
amirs <amirsabour874572@gmail.com>
2026-06-04 10:33:26 +0200

    Signals angewendet

--- frontend/team16-quiz-app/src/app/pages/register/register.ts
+++ frontend/team16-quiz-app/src/app/pages/register/register.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-register',
@@ -14,16 +15,21 @@ import { MatButtonModule } from '@angular/material/button';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressSpinnerModule
   ],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class Register {
+  isLoading = signal(false);
+  errorMessage = signal('');
+  successMessage = signal('');
+
   registerForm;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.registerForm = this.formBuilder.group({
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -31,11 +37,20 @@ export class Register {
   }
 
   onSubmit() {
+    this.errorMessage.set('');
+    this.successMessage.set('');
+
     if (this.registerForm.invalid) {
       this.registerForm.markAllAsTouched();
       return;
     }
 
+    this.isLoading.set(true);
+
+    // Später kommt hier API-Call rein
     console.log(this.registerForm.value);
+
+    this.successMessage.set('Registrierung vorbereitet.');
+    this.isLoading.set(false);
   }
 }
