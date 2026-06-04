7b6a7e9b60a21c4743a87592222de6a5fbe21906
Muhammed Budak <muhammedbudak2002@outlook.de>
2026-06-04 12:14:32 +0200

    home screen done

--- frontend/team16-quiz-app/src/app/app.routes.ts
+++ frontend/team16-quiz-app/src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register} from './pages/register/register';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'home',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
   {
     path: 'login',
     component: Login,
@@ -10,10 +20,5 @@ export const routes: Routes = [
   {
     path: 'register',
     component: Register,
-  },
-  {
-    path: '',
-    redirectTo: 'register',
-    pathMatch: 'full',
   },
 ];
