d7f5193aa673d0b94f85a353c0280e1f15881b5d
amirs <amirsabour874572@gmail.com>
2026-06-08 11:40:08 +0200

    lougout button hinzugefügt

--- frontend/team16-quiz-app/src/app/pages/home/home.ts
+++ frontend/team16-quiz-app/src/app/pages/home/home.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../auth/services/auth';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrl: './home.scss'
 })
-export class Home {}
+export class Home {
+  authService = inject(AuthService);
+
+  logout() {
+    this.authService.logout();
+    window.location.reload();
+  }
+}
