a0241767014a51db7aebe5ec5df207d12f1efa30
amirs <amirsabour874572@gmail.com>
2026-06-11 11:34:34 +0200

    Quiz-Setup fertig

--- frontend/team16-quiz-app/src/app/pages/quiz-setup/quiz-setup.ts
+++ frontend/team16-quiz-app/src/app/pages/quiz-setup/quiz-setup.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-quiz-setup',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterLink,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule
+  ],
   templateUrl: './quiz-setup.html',
-  styleUrl: './quiz-setup.scss',
+  styleUrl: './quiz-setup.scss'
 })
-export class QuizSetup {}
+export class QuizSetup {
+
+  category = '';
+
+  difficulty = '';
+}
