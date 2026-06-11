82f2f5e619de3fdf58d51848fa10372758612421
amirs <amirsabour874572@gmail.com>
2026-06-11 13:18:46 +0200

    Single-player-quiz fertig

--- frontend/team16-quiz-app/src/app/pages/single-player-quiz/single-player-quiz.ts
+++ frontend/team16-quiz-app/src/app/pages/single-player-quiz/single-player-quiz.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-single-player-quiz',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './single-player-quiz.html',
-  styleUrl: './single-player-quiz.scss',
+  styleUrl: './single-player-quiz.scss'
 })
-export class SinglePlayerQuiz {}
+export class SinglePlayerQuiz {
+  lives = signal(3);
+  score = signal(0);
+  selectedAnswer = signal('');
+
+  question = signal({
+    category: 'Sport',
+    difficulty: 'Einfach',
+    text: 'In welchem Sport wird ein Shuttlecock verwendet?',
+    answers: [
+      'Badminton',
+      'Rugby',
+      'Cricket',
+      'Tischtennis'
+    ],
+    correctAnswer: 'Badminton'
+  });
+
+  selectAnswer(answer: string): void {
+    this.selectedAnswer.set(answer);
+
+    if (answer === this.question().correctAnswer) {
+      this.score.update(score => score + 1);
+    } else {
+      this.lives.update(lives => lives - 1);
+    }
+  }
+}
